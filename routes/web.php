8d69c2febb319ee75b05e589c537b7f451d086bd
Robson Gomes Pedreira <154639339+techvanguard2023@users.noreply.github.com>
2026-07-16 09:28:34 -0300

    alteacao de removao do front

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => 'Meu Custo Total API',
+        'status' => 'ok',
+    ]);
 });
